1b47b3cb891e6f61b2a2771c3d70d70fc1a41486
Caique Santos <caisbrito2003@gmail.com>
2020-12-07 19:49:46 -0300

    first commit

---
+++ Heranca/Classes/Pessoa.cs
@@ -0,0 +1,11 @@
+namespace Heranca.Classes
+{
+    public class Pessoa
+    {
+        public string nome;
+        
+        public string darBoasVindas(string nomeUsuario){
+            return "Olá " + nomeUsuario + ", seja bem vindo(a) ao sistema";
+        }
+    }
+}
---
+++ Heranca/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Heranca.Classes;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PessoaFisica pf = new PessoaFisica();
+
+            pf.cpf = "359.082.438-76";   
+            pf.nome = "Caique";   
+
+            PessoaJuridica pj = new PessoaJuridica();
+            
+            pj.cnpj= "12.345.678/0001-30";
+           
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine( pf.darBoasVindas(pf.nome));
+            Console.WriteLine(pj.IsCNPJ (pj.cnpj));
+            Console.WriteLine (pf.IsCPF (pf.cpf));
+            Console.ResetColor();    
+        }
+    }
+}
